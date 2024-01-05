29934c74be1f82240574ca814d2df5a47deb51fa
Mitchell <tmitchell@cdsvisual.com>
2024-01-05 13:49:56 -0700

    added logic for svg shapes

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 let inquirer = require('inquirer');
 let fs = require('fs');
+const { find } = require('rxjs');
 
-const questions =  [{
+const questions = [{
     type: 'input',
     message: 'Enter text for logo (1-3 characters)',
     name: 'logo_text',
@@ -15,8 +16,8 @@ const questions =  [{
     type: 'list',
     message: 'Select a shape for logo',
     name: 'logo_shape',
-    choices: ['Circle','Triangle','Square']
-    
+    choices: ['Circle', 'Triangle', 'Square']
+
 },
 {
     type: 'input',
@@ -24,13 +25,27 @@ const questions =  [{
     name: 'logo_shape_color',
 },
 ]
-
+const shapeList = [
+    {
+        id: Circle,
+        value: `circle cx="150" cy="100" r="80"`
+    },
+    {
+        id: Square,
+        value: `rect x="50" y="10" width="200" height="200"`
+    },
+    {
+        id: Triangle,
+        value: `polygon points="150,10 0,160 300,160"`
+    }
+]
 svgstring = ""
-function createSVG () {
-    inquirer.prompt(questions).then((response)=>{
+function createSVG() {
+    inquirer.prompt(questions).then((response) => {
+        let shape = shapeList.find(shape => shape.id === response.logo_shape)
         svgstring += `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
 
-        <circle cx="150" cy="100" r="80" fill="green" />
+        <${shape} fill="${response.logo_shape_color}" />
       
         <text x="150" y="125" font-size="60" text-anchor="middle" fill="${response.logo_text_color}">${response.logo_text}</text>
       
