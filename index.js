aca9a1aa01df2a07fa0322eddb65d6c5fa0b0742
Mitchell <tmitchell@cdsvisual.com>
2024-01-05 14:03:27 -0700

    fixed error with getting shape info w/ find command

--- index.js
+++ index.js
@@ -27,22 +27,22 @@ const questions = [{
 ]
 const shapeList = [
     {
-        id: Circle,
-        value: `circle cx="150" cy="100" r="80"`
+        id: 'Circle',
+        value: `circle cx="150" cy="110" r="80"`
     },
     {
-        id: Square,
+        id: 'Square',
         value: `rect x="50" y="10" width="200" height="200"`
     },
     {
-        id: Triangle,
+        id: 'Triangle',
         value: `polygon points="150,10 0,160 300,160"`
     }
 ]
 svgstring = ""
 function createSVG() {
     inquirer.prompt(questions).then((response) => {
-        let shape = shapeList.find(shape => shape.id === response.logo_shape)
+        let shape = shapeList.find(shape => shape.id === response.logo_shape).value
         svgstring += `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
 
         <${shape} fill="${response.logo_shape_color}" />
@@ -50,8 +50,15 @@ function createSVG() {
         <text x="150" y="125" font-size="60" text-anchor="middle" fill="${response.logo_text_color}">${response.logo_text}</text>
       
       </svg>`
-    })
-    fs.writeFile('logo.svg', svgstring, (err) => {
+      fs.writeFile('logo.svg', svgstring, (err) => {
         err ? console.error(err) : console.log('successfully created SVG file')
     })
+    })
+  
 }
+
+function init(){
+    createSVG();
+}
+
+init();
