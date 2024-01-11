f4aada54a5f0aa6ab6c9c7de0424e2c289f05e51
tronmitch <theron.mitchell@gmail.com>
2024-01-10 22:53:15 -0700

    fixed char limit function, added class for shapes, added examples

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 let inquirer = require('inquirer');
 let fs = require('fs');
+let shapes = require('./lib/shapes')
 // const { find } = require('rxjs');
 const maxChar = require('inquirer-maxlength-input-prompt');
 inquirer.registerPrompt('maxChar_input', maxChar);
@@ -28,24 +29,28 @@ const questions = [{
     name: 'logo_shape_color',
 },
 ]
-const shapeList = [
-    {
-        id: 'Circle',
-        value: `circle cx="150" cy="110" r="80"`
-    },
-    {
-        id: 'Square',
-        value: `rect x="50" y="10" width="200" height="200"`
-    },
-    {
-        id: 'Triangle',
-        value: `polygon points="150,10 0,160 300,160"`
-    }
-]
+//Using and array replaced with class
+// const shapeList = [
+//     {
+//         id: 'Circle',
+//         value: `circle cx="150" cy="110" r="80"`
+//     },
+//     {
+//         id: 'Square',
+//         value: `rect x="50" y="10" width="200" height="200"`
+//     },
+//     {
+//         id: 'Triangle',
+//         value: `polygon points="150,10 0,160 300,160"`
+//     }
+// ]
+let x = shapes.myShapes;
+console.log(shapes);
 svgstring = ""
 function createSVG() {
     inquirer.prompt(questions).then((response) => {
-        let shape = shapeList.find(shape => shape.id === response.logo_shape).value
+        let svgName = `${response.logo_shape}_${response.logo_shape_color}_${response.logo_text}`
+        let shape = shapes.myShapes.find(shape => shape.id === response.logo_shape).value
         svgstring += `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
 
         <${shape} fill="${response.logo_shape_color}" />
@@ -53,7 +58,7 @@ function createSVG() {
         <text x="150" y="125" font-size="60" text-anchor="middle" fill="${response.logo_text_color}">${response.logo_text}</text>
       
       </svg>`
-      fs.writeFile('logo.svg', svgstring, (err) => {
+      fs.writeFile(`./example/${svgName}.svg`, svgstring, (err) => {
         err ? console.error(err) : console.log('successfully created SVG file')
     })
     })
